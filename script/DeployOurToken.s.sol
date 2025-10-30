2a5b24c8434c52f53591e714cd9585934dba3e75
John-Mukhwana <johnbradill67@gmail.com>
2025-10-30 07:35:10 +0300

    Add deployment script for OurToken contract

---
+++ script/DeployOurToken.s.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {OurToken} from "../src/OurToken.sol";
+contract DeployOurToken is Script{
+    function run() external{
+        vm.startBroadcast();
+    }
+}
