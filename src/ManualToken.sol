30920e48c44c224b0b12b064d5f9558ade40f250
John-Mukhwana <johnbradill67@gmail.com>
2025-10-30 07:00:46 +0300

    Add balanceOf function and initialize s_balances mapping in ManualToken contract

--- src/ManualToken.sol
+++ src/ManualToken.sol
@@ -4,6 +4,9 @@
 pragma solidity ^0.8.18;
 
 contract ManualToken{
+
+    mapping(address => uint256) private s_balances;
+
     function name() public pure returns(string memory) {
         return "ManualToken";
     }
@@ -19,4 +22,10 @@ contract ManualToken{
     function decimals() public pure returns(uint8){
         return 18;
     }
+
+    function balanceOf(address _owner) public view returns(uint256){
+        return s_balances[_owner];
+    }
+
+    
 }
