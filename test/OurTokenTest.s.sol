f9cc2fe22abb93b5bb62a30bbee7337e8253f109
John-Mukhwana <johnbradill67@gmail.com>
2025-10-31 06:30:01 +0300

    Add setup function to OurTokenTest contract

--- test/OurTokenTest.s.sol
+++ test/OurTokenTest.s.sol
@@ -4,3 +4,7 @@ pragma solidity ^0.8.18;
 
 import {Test} from "lib/forge-std/src/Test.sol";
 import {DeployOurToken} from "../script/DeployOurToken.s.sol";
+
+contract OurTokenTest is Test{
+    function setUp() public{}
+}
